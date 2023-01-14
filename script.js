db01976acb5184693ab09b95d0597ec70217413b
Nick <nikolairastorguev6@gmail.com>
2023-01-14 23:28:47 +0500

    ir

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelector('.t'),
+    btn2 = document.querySelector('.i')
+
+btn1.addEventListener('click' , () => {
+    alert('Не обманывай ты спишь')
+})
+
+btn2.addEventListener('click', () => {
+    alert('Да я знаю что спишь, даже не поняла как я так быстро для тебя сайт сделал)))')
+})
